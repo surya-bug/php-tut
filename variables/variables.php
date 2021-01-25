0a9998dcbc7ade8cd23e080723e6a28517843775
surya-bug <77871514+surya-bug@users.noreply.github.com>
2021-01-25 22:37:03 +0530

    Create variables.php
    Added variables.php !

---
+++ variables/variables.php
@@ -0,0 +1,21 @@
+<?php
+
+$name = "Surya";
+
+$age = 25;
+
+$pi = 3.142;
+
+$is_awesome = true // or false;
+
+$data = null;
+
+var_dump($name);
+var_dump($age);
+var_dump($pi);
+
+var_dump($is_awesome,$data);
+
+
+
+?>
